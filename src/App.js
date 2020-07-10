c0cdd4bc887be2b1f4c94a21850c6edae933960d
Tunvir Rahman <tunvir.rahman07@gmail.com>
2020-07-10 22:45:11 +0600

    navigation added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import AppNavigation from './navigation/navigation';
+
+class App extends React.Component {
+  render() {
+    return (
+      <AppNavigation />
+    );
+  }
+}
+
+export default App;
