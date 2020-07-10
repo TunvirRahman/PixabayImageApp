6fdce6077c6e9095664e62eaab3d23507ed7bc3e
Tunvir Rahman <tunvir.rahman07@gmail.com>
2020-07-10 20:40:15 +0600

    http core service

--- src/service/core/httpService.js
+++ src/service/core/httpService.js
@@ -1,7 +1,13 @@
+import axios from 'axios';
 import API from '../../contants/appConstants';
 
 export default class HTTPService {
     constructor() {
         this.baseURL = API.BASE_URL;
+    }
+
+    get(endpoint, config) {
+        const url = this.baseURL + endpoint;
+        return axios.get(url, config);
     }
 }
--- src/service/core/httpService.test.js
+++ src/service/core/httpService.test.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import HttpService from './httpService';
 
 describe('httpService test', () => {
@@ -9,6 +10,35 @@ describe('httpService test', () => {
         it('baseURl is set properly', () => {
             const expectedBaseURL = 'https://pixabay.com/api';
             expect(service.baseURL).toEqual(expectedBaseURL);
+        });
+    });
+    describe('get method test', () => {
+        let service;
+        beforeAll(() => {
+            service = new HttpService();
+        });
+        it('get defined', () => {
+            expect(service.get).toBeDefined();
+        });
+        it('get toHaveBeenCalledWith mockEndpoint and config', () => {
+            jest.spyOn(axios, 'get');
+            const mockEndpoint = '/endpoint';
+            service.get(mockEndpoint, {});
+            expect(axios.get).toHaveBeenCalledWith(service.baseURL + mockEndpoint, {});
+        });
+        it('get ', () => {
+            const mockResponse = {
+                data: {
+                    data: {
+                        value: 'some mock response',
+                    },
+                },
+            };
+            jest.spyOn(axios, 'get').mockResolvedValueOnce(mockResponse);
+            expect.assertions(1);
+            service.get('/mockEndpoint', {}).then((response) => {
+                expect(response.data.data.value).toEqual('some mock response');
+            });
         });
     });
 });
